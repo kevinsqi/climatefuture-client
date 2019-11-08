616f59f2f4e19f8d80e91e657d06bab05f85d5d4
Kevin Qi <iqnivek@gmail.com>
2019-11-07 16:35:48 -0800

    refactor temperature attributes

--- components/LocationPage/Temperature.js
+++ components/LocationPage/Temperature.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import _ from 'lodash';
 
 import { CHART_SCALES } from './constants';
 import RelativeResult from './RelativeResult';
 
-function SectionItem({ label, description, children }) {
+function Item({ label, description, children }) {
   return (
     <div className="mt-4">
       <h4>{label}</h4>
@@ -13,46 +14,71 @@ function SectionItem({ label, description, children }) {
   );
 }
 
-function AverageTempItem({ result }) {
+function AverageTemperature({ result }) {
   return (
-    <SectionItem label="How could the temperature change?">
+    <Item label="How could the temperature change?">
       <RelativeResult result={result} unit="°F" chartScale={CHART_SCALES.temperature} />
-    </SectionItem>
+    </Item>
   );
 }
+AverageTemperature.attribute = 'temp_avg';
 
 function NumDaysAbove90F({ result }) {
   return (
-    <SectionItem label="How many days could be hotter than 90°F?">
+    <Item label="How many days could be hotter than 90°F?">
       <RelativeResult result={result} unit="days" chartScale={CHART_SCALES.year} />
-    </SectionItem>
+    </Item>
   );
 }
+NumDaysAbove90F.attribute = 'temp_num_days_above_90f';
 
 function NumDaysAbove100F({ result }) {
   return (
-    <SectionItem label="How many days could be hotter than 100°F?">
+    <Item label="How many days could be hotter than 100°F?">
       <RelativeResult result={result} unit="days" chartScale={CHART_SCALES.year} />
-    </SectionItem>
+    </Item>
   );
 }
+NumDaysAbove100F.attribute = 'temp_num_days_above_100f';
 
-export default function Temperature(props) {
-  const { temp_num_days_above_90f, temp_num_days_above_100f, temp_avg } = props.results;
-  if (!temp_num_days_above_90f && !temp_num_days_above_100f && !temp_avg) {
+const RESULT_COMPONENTS = {
+  AverageTemperature,
+  NumDaysAbove90F,
+  NumDaysAbove100F,
+};
+
+function Result({ result }) {
+  const Component = _.find(RESULT_COMPONENTS, (comp) => comp.attribute === result.attribute);
+
+  if (!Component) {
+    throw new Error(`No component for attribute ${result.attribute}`);
+  }
+
+  return <Component result={result} />;
+}
+
+export default function Temperature({ results }) {
+  const attributes = ['temp_avg', 'temp_num_days_above_90f', 'temp_num_days_above_100f'];
+
+  if (attributes.every((attr) => !results[attr])) {
     return null;
   }
+
   // TODO: methodology
   const showAdvice =
-    temp_num_days_above_100f &&
-    temp_num_days_above_100f.rcp85_mean - temp_num_days_above_100f.historical_average > 10;
+    results.temp_num_days_above_100f &&
+    results.temp_num_days_above_100f.rcp85_mean -
+      results.temp_num_days_above_100f.historical_average >
+      10;
+
   return (
     <div>
       <h3 className="h1 font-weight-bold">🔥 Temperature</h3>
 
-      <AverageTempItem result={temp_avg} />
-      <NumDaysAbove90F result={temp_num_days_above_90f} />
-      <NumDaysAbove100F result={temp_num_days_above_100f} />
+      {attributes.map((attr) => {
+        const result = results[attr];
+        return <Result result={result} key={attr} />;
+      })}
       {showAdvice && (
         <div className="mt-4">
           <h4>How can I prepare?</h4>
